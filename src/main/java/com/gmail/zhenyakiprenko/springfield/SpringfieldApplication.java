eb5ad13cc70be7ec17f48784ad7636bfc5634a03
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2021-01-02 06:12:41 +0200

    springfield - init commit

---
+++ src/main/java/com/gmail/zhenyakiprenko/springfield/SpringfieldApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.zhenyakiprenko.springfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/zhenyakiprenko/springfield/SpringfieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.zhenyakiprenko.springfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
