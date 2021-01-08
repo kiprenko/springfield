d4cbd68af8f2d51bb011783be3af1f130bc5e035
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2021-01-08 02:32:46 +0200

    springfield - upgraded UserNotFoundException.

--- src/main/java/com/kiprenko/springfield/exception/UserNotFoundException.java
+++ src/main/java/com/kiprenko/springfield/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.kiprenko.springfield.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found in the database")
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
     }
