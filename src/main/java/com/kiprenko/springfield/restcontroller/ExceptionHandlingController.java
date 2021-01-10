88d859b50107c5baa29830f406ce5ada77c4b78a
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2021-01-10 03:29:41 +0200

    springfield - refactored ExceptionHandlingController.

--- src/main/java/com/kiprenko/springfield/restcontroller/ExceptionHandlingController.java
+++ src/main/java/com/kiprenko/springfield/restcontroller/ExceptionHandlingController.java
@@ -1,6 +1,8 @@
 package com.kiprenko.springfield.restcontroller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlingController {
 
     @ExceptionHandler(Exception.class)
-    public void handleError(Exception ex) {
-        LOGGER.error(ex.getClass().getName() + " occurred.", ex);
+    public ResponseEntity<Object> handleException(Exception ex) {
+        LOGGER.error(ex);
+        return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
