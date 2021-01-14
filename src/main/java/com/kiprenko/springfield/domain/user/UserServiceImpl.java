cddc54c2795bfbc8afe0657509496f9bece6eaae
Evgeniy Kiprenko <zhenyakipenko@gmail.com>
2021-01-14 23:20:25 +0200

    springfield-security-jwt - refactoring.

--- src/main/java/com/kiprenko/springfield/domain/user/UserServiceImpl.java
+++ src/main/java/com/kiprenko/springfield/domain/user/UserServiceImpl.java
@@ -11,16 +11,20 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
+    public static final String USER_NOT_FOUND_BY_ID_TEMPLATE = "User with ID = %d wasn't found";
     private final UserRepository repository;
     private final UserMapper userMapper;
     private final int defaultPageSize;
+    private final int maxPageSize;
 
     public UserServiceImpl(UserRepository repository,
                            UserMapper userMapper,
-                           @Value("${usersListDefaultPageSize}") int defaultPageSize) {
+                           @Value("${application.usersListDefaultPageSize}") int defaultPageSize,
+                           @Value("${application.usersListMaxPageSize}") int maxPageSize) {
         this.repository = repository;
         this.userMapper = userMapper;
         this.defaultPageSize = defaultPageSize;
+        this.maxPageSize = maxPageSize;
     }
 
     @Override
@@ -32,7 +36,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserInfoProjection get(long id) {
         assertId(id, "Can't get a user by ID less than 1. ID = %d");
-        return repository.findProjectionById(id).orElseThrow(UserNotFoundException::new);
+        return repository.findProjectionById(id)
+                .orElseThrow(() -> new UserNotFoundException(String.format(USER_NOT_FOUND_BY_ID_TEMPLATE, id)));
     }
 
     private void assertId(long id, String msg) {
@@ -44,7 +49,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserInfoProjection get(String username) {
         assertUsername(username);
-        return repository.findProjectionByUsername(username).orElseThrow(UserNotFoundException::new);
+        return repository.findProjectionByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(String.format("User with username = %s wasn't found", username)));
     }
 
     @Override
@@ -65,14 +71,23 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<UserInfoProjection> getList(int page, int pageSize) {
-        return repository.findAllProjectionsBy(PageRequest.of(page, pageSize));
+    public List<UserInfoProjection> getList(int pageNumber, int pageSize) {
+        if (pageNumber < 0) {
+            throw new IllegalArgumentException("Page number can't be less than zero. Page number = " + pageNumber);
+        }
+        if (pageSize < 0 || pageSize > maxPageSize) {
+            throw new IllegalArgumentException(String.format("Page size can't be less than zero or greater than %d. Page size = %d", maxPageSize, pageSize));
+        }
+        return repository.findAllProjectionsBy(PageRequest.of(pageNumber, pageSize));
     }
 
     @Override
     public void updateInfo(UserDto user) {
         assertUserDto(user, "Can't update a user info when user is null");
-        User persistedUser = repository.findById(user.getId()).orElseThrow(UserNotFoundException::new);
+        Long id = user.getId();
+        User persistedUser = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(String.format(USER_NOT_FOUND_BY_ID_TEMPLATE, id)));
+
         Optional.ofNullable(user.getFirstName())
                 .filter(s -> !s.isBlank())
                 .ifPresent(persistedUser::setFirstName);
@@ -96,7 +111,7 @@ public class UserServiceImpl implements UserService {
             throw new IllegalArgumentException(String.format("Can't update password for user with ID = %d. New password is null or blank", id));
         }
         User persistedUser = repository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(String.format("User with ID = %d wasn't found", id)));
+                .orElseThrow(() -> new UserNotFoundException(String.format(USER_NOT_FOUND_BY_ID_TEMPLATE, id)));
         repository.save(persistedUser);
     }
 
--- src/main/java/com/kiprenko/springfield/restcontroller/v1/UserManageController.java
+++ src/main/java/com/kiprenko/springfield/restcontroller/v1/UserManageController.java
@@ -43,20 +43,10 @@ public class UserManageController {
     @GetMapping(value = "/list", produces = APPLICATION_JSON_VALUE)
     public List<UserInfoProjection> getUsersList(@RequestParam Integer pageNumber,
                                                  @RequestParam(required = false) Integer pageSize) {
-        AssertUsersListParameters(pageNumber, pageSize);
         if (pageSize == null) {
             return userService.getList(pageNumber);
         }
         return userService.getList(pageNumber, pageSize);
-    }
-
-    private void AssertUsersListParameters(Integer pageNumber, Integer pageSize) {
-        if (pageNumber == null || pageNumber < 0) {
-            throw new IllegalArgumentException("Page number can't be null or less than zero. Page number = " + pageNumber);
-        }
-        if (pageSize != null && pageSize < 0) {
-            throw new IllegalArgumentException("Page size can't be less than zero. Page size = " + pageSize);
-        }
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
